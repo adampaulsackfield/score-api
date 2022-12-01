6276801b53fe6d781745846b655c6882760c1b7b
Adam Sackfield <adamsackfield20@gmail.com>
2022-12-01 01:31:13 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./db/connections');
+
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 5550;
+
+connectDB();
+
+const server = express();
+
+server.use(express.json());
+// server.use(cors());
+
+server.use('/api', routes);
+
+server.get('/', (req, res) => {
+	res.status(200).send({ message: 'Running' });
+});
+
+server.all('*', (req, res) => {
+	res.status(404).send({ message: 'Path not found' });
+});
+
+server.use(
+	cors({
+		origin: [
+			'http://localhost',
+			'http://localhost:5501',
+			'https://adampaulsackfield.github.io/adampaulsackfield.github.io-snake/',
+		],
+	})
+);
+
+server.listen(PORT);
